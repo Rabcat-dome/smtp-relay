537ccbc1bded3c8abf476f078f29f358379d906d
Visarut Phartchayanusit <phvisarut@centralpattana.co.th>
2024-07-07 17:15:20 +0700

    Test Html Format

--- doc/dotnet8-test-sendmail/xunit-smtp/EmailSenderTests.cs
+++ doc/dotnet8-test-sendmail/xunit-smtp/EmailSenderTests.cs
@@ -6,17 +6,40 @@ namespace xunit_smtp
     public class EmailSenderTests
     {
         [Fact]
-        public async Task TestSendEmail()
+        public async Task TestSendPlainEmail()
         {
             // Arrange
             var emailMessage = new MimeMessage();
             emailMessage.From.Add(new MailboxAddress("Test Sender", "test@example.com"));
             emailMessage.To.Add(new MailboxAddress("Test Recipient", "recipient@example.com"));
-            emailMessage.Subject = "Test Email from xUnit";
+            emailMessage.Subject = "Test Email plain format from xUnit";
             emailMessage.Body = new TextPart("plain")
             {
                 Text = "This is a test email sent from xUnit using Mailhog."
             };
+
+            // Act
+            var result = await SendEmailAsync(emailMessage);
+
+            // Assert
+            Assert.True(result, "The email was not sent successfully.");
+        }
+
+        [Fact]
+        public async Task TestSendHtmlEmail()
+        {
+            // Arrange
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("Test Sender", "test@example.com"));
+            emailMessage.To.Add(new MailboxAddress("Test Recipient", "recipient@example.com"));
+            emailMessage.Subject = "Test HTML Email from xUnit";
+
+            var bodyBuilder = new BodyBuilder
+            {
+                HtmlBody = "<html><body><h1>This is a test email</h1><p>Sent from <b>xUnit</b> using <i>Mailhog</i>.</p></body></html>",
+                TextBody = "This is a test email sent from xUnit using Mailhog."
+            };
+            emailMessage.Body = bodyBuilder.ToMessageBody();
 
             // Act
             var result = await SendEmailAsync(emailMessage);
